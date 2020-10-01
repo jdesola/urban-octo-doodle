bd24d356a9edbe72c188d5d7ce8fe1255a2e421b
Brian Lauvray <lauvray.1@osu.edu>
2020-10-01 18:21:02 -0400

    Added TDD

---
+++ module-1/15_Test_Driven_Development/student-lecture/src/main/java/com/techelevator/Less20.java
@@ -0,0 +1,14 @@
+package com.techelevator;
+
+
+/*
+	Return true if the given number is 1 or 2 less than a multiple of 20. 
+	So for example 38 and 39 return true, but 40 returns false.
+	less20(18) → true
+	less20(19) → true
+	less20(20) → false
+*/
+public class Less20 {
+
+
+}
