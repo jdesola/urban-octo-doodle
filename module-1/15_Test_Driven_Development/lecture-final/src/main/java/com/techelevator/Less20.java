41206443ad1889bf24de66e6eba0f92e9117551b
Brian Lauvray <brian@techelevator.com>
2020-10-02 11:05:00 -0400

    Refactored

--- module-1/15_Test_Driven_Development/lecture-final/src/main/java/com/techelevator/Less20.java
+++ module-1/15_Test_Driven_Development/lecture-final/src/main/java/com/techelevator/Less20.java
@@ -12,13 +12,11 @@ public class Less20 {
 
 	public boolean isLessThanMultipleOf20(int n) {
 		
-		if ( n % 20 == 19) {
+		if ( n % 20 == 19 || n % 20 == 18) {
 			return true;
 		}
 		
-		if ( n % 20 == 18 ) {
-			return true;
-		}
+
 		
 		return false;
 	}
