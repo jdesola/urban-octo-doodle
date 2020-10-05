9e7c84e924a5c3254a9f2cda2df90430ba19877f
Josh DeSola <joshdesola@gmail.com>
2020-10-05 00:32:19 -0400

    Refactoring and cleanup

--- module-1/15_Test_Driven_Development/student-exercise/src/main/java/com/techelevator/KataFizzBuzz.java
+++ module-1/15_Test_Driven_Development/student-exercise/src/main/java/com/techelevator/KataFizzBuzz.java
@@ -9,18 +9,12 @@ public class KataFizzBuzz {
 			fizzBuzzString = "Fizz";
 		} 
 		if ((num % 5 == 0) || ( num.toString().contains("5")) ) {
-			fizzBuzzString = "Buzz";
-		} 
-		if ( (num % 3 == 0 && num % 5 == 0) || ( num.toString().contains("3") && num.toString().contains("5")) ) {
-			fizzBuzzString = "FizzBuzz";
+			fizzBuzzString += "Buzz";
 		} 
 		if (num >= 1 && num <= 100 && (num % 3 != 0 && num % 5 != 0) && !( num.toString().contains("3") ) && !( num.toString().contains("5")) ) {
 			fizzBuzzString = num.toString();
 		}
-		if (num < 1) {
-			fizzBuzzString = "";
-		}
-		if (num > 100) {
+		if (num < 1 || num > 100) {
 			fizzBuzzString = "";
 		}
 		
