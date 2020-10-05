9c522fc1ab9448b58b4e3c601cf146701128d541
Josh DeSola <joshdesola@gmail.com>
2020-10-04 23:35:46 -0400

    Add test method fizzBuzzReturnsFizzForMultipleThree

--- module-1/15_Test_Driven_Development/student-exercise/src/test/java/com/techelevator/KataFizzBuzzTest.java
+++ module-1/15_Test_Driven_Development/student-exercise/src/test/java/com/techelevator/KataFizzBuzzTest.java
@@ -6,5 +6,17 @@ import static org.junit.Assert.*;
 
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class KataFizzBuzzTest {
-
+	private KataFizzBuzz kataFizzBuzz;
+	
+	@Before
+	public void setup() {
+		kataFizzBuzz = new KataFizzBuzz();
+	}
+	
+	@Test
+	public void fizzBuzz_returns_Fizz_for_multiple_3() {
+		int fizzBuzzTester = 24;
+		Assert.assertEquals("Fizz", kataFizzBuzz.fizzBuzz(fizzBuzzTester));
+	}
+	
 }
