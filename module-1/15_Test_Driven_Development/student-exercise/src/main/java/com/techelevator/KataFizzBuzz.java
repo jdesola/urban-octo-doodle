f8355464c2cdc636846140394d40b1fc1776e3d6
Josh DeSola <joshdesola@gmail.com>
2020-10-05 00:06:36 -0400

    Ad passing code for test method fizzBuzzReturnsEmptyStringIfInputNotBetween1And100

--- module-1/15_Test_Driven_Development/student-exercise/src/main/java/com/techelevator/KataFizzBuzz.java
+++ module-1/15_Test_Driven_Development/student-exercise/src/main/java/com/techelevator/KataFizzBuzz.java
@@ -3,20 +3,27 @@ package com.techelevator;
 public class KataFizzBuzz {
 	
 	public String fizzBuzz(Integer num) {
-		String fizzBuzzString = num.toString();
+		String fizzBuzzString = "";
+				
 		if (num % 3 == 0) {
 			fizzBuzzString = "Fizz";
-		}
-		
+		} 
 		if (num % 5 == 0) {
 			fizzBuzzString = "Buzz";
-		}
-		
+		} 
 		if (num % 3 == 0 && num % 5 == 0) {
 			fizzBuzzString = "FizzBuzz";
+		} 
+		if (num >= 1 && num <= 100 && (num % 3 != 0 && num % 5 != 0)) {
+			fizzBuzzString = num.toString();
+		}
+		if (num < 1) {
+			fizzBuzzString = "";
+		}
+		if (num > 100) {
+			fizzBuzzString = "";
 		}
 		
 		return fizzBuzzString;
 	}
-
 }
