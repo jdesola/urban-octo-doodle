05e59880963e1c5cd37544399087612025c56469
Josh DeSola <joshdesola@gmail.com>
2020-10-05 13:26:41 -0400

    Completed Part 1 of Word Search

--- module-1/16_FileIO_Reading_in/student-exercise/src/main/java/com/techelevator/WordSearch.java
+++ module-1/16_FileIO_Reading_in/student-exercise/src/main/java/com/techelevator/WordSearch.java
@@ -1,9 +1,36 @@
 package com.techelevator;
 
+import java.io.*;
+import java.util.Scanner;
+
 public class WordSearch {
 
 	public static void main(String[] args) {
-	
+		Scanner in = new Scanner(System.in);
+		
+		System.out.println("What is the file that should be searched?");
+		
+		String filePath = in.nextLine();
+		
+		System.out.println("What is the search word you are looking for?");
+		
+		String searchWord = in.nextLine();
+		
+		File fileToSearch = new File( filePath );
+		
+		int lineNumber = 1;
+		
+			try(Scanner fileScanner = new Scanner( fileToSearch )) {
+				while (fileScanner.hasNextLine()) {
+					String lineToSearch = fileScanner.nextLine();
+					if (lineToSearch.contains(searchWord)) {
+						System.out.printf("%d) %s\n", lineNumber, lineToSearch);
+					}
+					lineNumber++;
+				}
+			} catch (FileNotFoundException ex){
+				System.out.println("Cannot find file");
+		}
 
 	}
 
