e26e63e625b41ba59e327da9a9b2c3a9e216ff4e
Josh DeSola <joshdesola@gmail.com>
2020-10-06 18:41:51 -0400

    Submitting FizzWriter Exercise

--- module-1/17_FileIO_Writing_out/student-exercise/src/main/java/com/techelevator/FizzWriter.java
+++ module-1/17_FileIO_Writing_out/student-exercise/src/main/java/com/techelevator/FizzWriter.java
@@ -1,9 +1,36 @@
 package com.techelevator;
 
+import java.io.*;
+
 public class FizzWriter {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
+		String filePath = "/Users/student/Development/joshuadesola-java-blue-material/module-1/17_FileIO_Writing_out/student-exercise";
+		String fizzNumString = "300";
+		String fizzBuzzName = "FizzBuzz.txt";
+		String fileName = fizzNumString + " " + fizzBuzzName;
 
+		File fizzBuzzFile = new File(filePath, fileName);
+		fizzBuzzFile.createNewFile();
+
+
+		try (PrintWriter writer = new PrintWriter(fizzBuzzFile)) {
+			for (Integer i = 1; i <= Integer.parseInt(fizzNumString); i++) {
+				String fizzBuzz = "";
+				if (i % 3 == 0) {
+					fizzBuzz += "Fizz";
+				} 
+				if (i % 5 == 0) {
+					fizzBuzz += "Buzz";
+				} 
+				if (!( i % 3 == 0) && !( i % 5 == 0)) {
+					fizzBuzz = i.toString();
+				}
+				writer.println(fizzBuzz);
+				writer.flush();
+			}
+		}
+		System.out.println(fizzBuzzName + " has been created.");
 	}
 
 }
