7b64f816a4130fd01b0143b283a16263ff750d1e
Brian Lauvray <brian@techelevator.com>
2020-10-30 12:52:16 -0400

    Review Lecture code

---
+++ module-3/00-Review-Project-1/lecture-final/client/src/main/java/com/techelevator/contacts/model/Contact.java
@@ -0,0 +1,32 @@
+package com.techelevator.contacts.model;
+
+public class Contact {
+
+	private int contactId;
+	private String firstName;
+	private String lastName;
+	
+	public int getContactId() {
+		return contactId;
+	}
+	public void setContactId(int contactId) {
+		this.contactId = contactId;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	
+	public String getFullName() {
+		return this.firstName + " " + this.lastName;
+	}
+	
+}
