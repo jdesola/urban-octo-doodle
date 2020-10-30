f92d17f1f23f36afc84d31f54916f67b55252d42
Brian Lauvray <lauvray.1@osu.edu>
2020-10-29 21:37:00 -0400

    Added Review Project

---
+++ module-3/00-Review-Project-1/lecture-final/client/src/main/java/com/techelevator/contacts/services/ConsoleService.java
@@ -0,0 +1,13 @@
+package com.techelevator.contacts.services;
+
+import java.util.Scanner;
+
+public class ConsoleService {
+
+  private Scanner scanner;
+
+  public ConsoleService() {
+    scanner = new Scanner(System.in);
+  }
+
+}
