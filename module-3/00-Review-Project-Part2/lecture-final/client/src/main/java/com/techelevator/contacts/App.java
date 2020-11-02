ef271704c73c598cf7fd86736fa5ef040343423a
Brian Lauvray <lauvray.1@osu.edu>
2020-11-02 16:39:08 -0500

    Added m3 Review Day 2

---
+++ module-3/00-Review-Project-Part2/lecture-final/client/src/main/java/com/techelevator/contacts/App.java
@@ -0,0 +1,137 @@
+package com.techelevator.contacts;
+
+import java.util.List;
+
+import org.springframework.web.client.RestClientResponseException;
+
+import com.techelevator.contacts.model.AuthenticatedUser;
+import com.techelevator.contacts.model.Contact;
+import com.techelevator.contacts.model.UserCredentials;
+import com.techelevator.contacts.services.AuthenticationService;
+import com.techelevator.contacts.services.AuthenticationServiceException;
+import com.techelevator.contacts.services.ContactService;
+import com.techelevator.view.ConsoleService;
+
+public class App {
+
+private static final String API_BASE_URL = "http://localhost:8080/";
+    
+    private static final String MENU_OPTION_EXIT = "Exit";
+    private static final String LOGIN_MENU_OPTION_REGISTER = "Register";
+	private static final String LOGIN_MENU_OPTION_LOGIN = "Login";
+	private static final String[] LOGIN_MENU_OPTIONS = { LOGIN_MENU_OPTION_REGISTER, LOGIN_MENU_OPTION_LOGIN, MENU_OPTION_EXIT };
+	
+	private static final String MAIN_MENU_OPTION_LIST_CONTACTS = "List Contacts";
+	private static final String MAIN_MENU_OPTION_LOGIN = "Login as different user";
+	private static final String[] MAIN_MENU_OPTIONS = { MAIN_MENU_OPTION_LIST_CONTACTS, MAIN_MENU_OPTION_LOGIN, MENU_OPTION_EXIT };
+	
+    private AuthenticatedUser currentUser;
+    private ConsoleService console;
+    private AuthenticationService authenticationService;
+    private ContactService contactService;
+    
+    public static void main(String[] args) {
+    	App app = new App(new ConsoleService(System.in, System.out), new AuthenticationService(API_BASE_URL));
+    	app.run();
+    }
+
+    public App(ConsoleService console, AuthenticationService authenticationService) {
+		this.console = console;
+		this.authenticationService = authenticationService;
+	}
+
+	public void run() {
+		System.out.println("*********************");
+		System.out.println("* Welcome to Contacts! *");
+		System.out.println("*********************");
+	
+		registerAndLogin();
+	
+		this.contactService = new ContactService(API_BASE_URL);
+		
+		mainMenu();
+	}
+
+	private void mainMenu() {
+		while(true) {
+			String choice = (String)console.getChoiceFromOptions(MAIN_MENU_OPTIONS);
+			if(MAIN_MENU_OPTION_LIST_CONTACTS.equals(choice)) {
+				listAllContacts();
+			} else if(MAIN_MENU_OPTION_LOGIN.equals(choice)) {
+				login();
+			} else {
+				// the only other option on the main menu is to exit
+				exitProgram();
+			}
+		}
+	}
+
+	private void listAllContacts() {
+		try {
+			List<Contact> contacts = contactService.list();
+			console.printContacts(contacts);
+		}  catch (RestClientResponseException e) {
+			console.printRestException(e);
+		}
+	}
+	
+	private void exitProgram() {
+		System.exit(0);
+	}
+
+	private void registerAndLogin() {
+		while(!isAuthenticated()) {
+			String choice = (String)console.getChoiceFromOptions(LOGIN_MENU_OPTIONS);
+			if (LOGIN_MENU_OPTION_LOGIN.equals(choice)) {
+				login();
+			} else if (LOGIN_MENU_OPTION_REGISTER.equals(choice)) {
+				register();
+			} else {
+				// the only other option on the login menu is to exit
+				exitProgram();
+			}
+		}
+	}
+
+	private boolean isAuthenticated() {
+		return currentUser != null;
+	}
+
+	private void register() {
+		System.out.println("Please register a new user account");
+		boolean isRegistered = false;
+        while (!isRegistered) //will keep looping until user is registered
+        {
+            UserCredentials credentials = collectUserCredentials();
+            try {
+            	authenticationService.register(credentials);
+            	isRegistered = true;
+            	System.out.println("Registration successful. You can now login.");
+            } catch(AuthenticationServiceException e) {
+            	System.out.println("REGISTRATION ERROR: "+e.getMessage());
+				System.out.println("Please attempt to register again.");
+            }
+        }
+	}
+
+	private void login() {
+		System.out.println("Please log in");
+		currentUser = null;
+		while (currentUser == null) //will keep looping until user is logged in
+		{
+			UserCredentials credentials = collectUserCredentials();
+		    try {
+				currentUser = authenticationService.login(credentials);
+			} catch (AuthenticationServiceException e) {
+				System.out.println("LOGIN ERROR: "+e.getMessage());
+				System.out.println("Please attempt to login again.");
+			}
+		}
+	}
+	
+	private UserCredentials collectUserCredentials() {
+		String username = console.getUserInput("Username");
+		String password = console.getUserInput("Password");
+		return new UserCredentials(username, password);
+	}
+}
---
+++ module-3/00-Review-Project-Part2/student-lecture/client/src/main/java/com/techelevator/contacts/services/ContactService.java
@@ -0,0 +1,24 @@
+package com.techelevator.contacts.services;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.web.client.RestTemplate;
+
+import com.techelevator.contacts.model.Contact;
+
+public class ContactService {
+
+	private static String API_URL;
+	private static final RestTemplate restTemplate = new RestTemplate();
+	
+	public ContactService(String url) {
+		this.API_URL = url + "contacts";
+	}
+	
+	public List<Contact> list() {
+		Contact[] contacts = restTemplate.getForObject(API_URL, 
+				Contact[].class);
+		return Arrays.asList(contacts);
+	}
+}
