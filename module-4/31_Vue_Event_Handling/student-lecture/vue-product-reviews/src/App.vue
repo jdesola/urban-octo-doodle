b12a35335485012c2d4c0bb0044d57a288f687c6
Brian Lauvray <lauvray.1@osu.edu>
2020-11-22 13:11:55 -0500

    Added Vue Event Handling lecture and Component Communication tututorial

---
+++ module-4/31_Vue_Event_Handling/student-lecture/vue-product-reviews/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <product-review></product-review>
+    <add-email></add-email>
+  </div>
+</template>
+
+<script>
+import ProductReview from './components/ProductReview'
+import AddEmail from './components/AddEmail'
+
+export default {
+  name: 'App',
+  components: {
+    ProductReview,
+    AddEmail
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  max-width: 800px;
+  margin: 60px auto 0 auto;
+}
+</style>
