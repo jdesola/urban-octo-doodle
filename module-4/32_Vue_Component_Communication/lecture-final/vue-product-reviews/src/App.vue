0fd36364744cc04f40236586a6571e0d7dac3ae8
Brian Lauvray <brian@techelevator.com>
2020-11-24 12:37:32 -0500

    VueX lecture code

--- module-4/32_Vue_Component_Communication/lecture-final/vue-product-reviews/src/App.vue
+++ module-4/32_Vue_Component_Communication/lecture-final/vue-product-reviews/src/App.vue
@@ -1,16 +1,35 @@
 <template>
-  <div id="app">
-    <product-review></product-review>
+  <div id="app" class="main">
+    <h1>Product Reviews for {{ $store.state.name }}</h1>
+    <p class="description">{{ $store.state.description }}</p>
+    <div class="well-display">
+      <average-summary />
+      <star-summary rating="1" />
+      <star-summary rating="2" />
+      <star-summary rating="3" />
+      <star-summary rating="4" />
+      <star-summary rating="5" />
+    </div>
+    <add-review />
+    <review-list />
+    
   </div>
 </template>
 
 <script>
-import ProductReview from './components/ProductReview'
+import AverageSummary from "./components/AverageSummary"
+import ReviewList from './components/ReviewList.vue'
+import StarSummary from './components/StarSummary.vue'
+import AddReview from './components/AddReview.vue'
 
 export default {
   name: 'App',
   components: {
-    ProductReview,
+    AverageSummary,
+    StarSummary,
+    ReviewList,
+    AddReview
+
   }
 }
 </script>
@@ -25,4 +44,87 @@ export default {
   max-width: 800px;
   margin: 60px auto 0 auto;
 }
+div.main {
+    margin: 1rem 0;
+}
+div.main div.well-display {
+    display: flex;
+    justify-content: space-around;
+}
+
+div.main div.well-display div.well {
+    display: inline-block;
+    width: 15%;
+    border: 1px black solid;
+    border-radius: 6px;
+    text-align: center;
+    margin: 0.25rem;
+}
+
+div.main div.well-display div.well span.amount {
+    color: darkslategray;
+    display: block;
+    font-size: 2.5rem;
+}
+
+div.main div.review {
+    border: 1px black solid;
+    border-radius: 6px;
+    padding: 1rem;
+    margin: 10px;
+}
+
+div.main div.review div.rating {
+    height: 2rem;
+    display: inline-block;
+    vertical-align: top;
+    margin: 0 0.5rem;
+}
+
+div.main div.review div.rating img {
+    height: 100%;
+}
+
+div.main div.review p {
+    margin: 20px;
+}
+
+div.main div.review h3 {
+    display: inline-block;
+}
+
+div.main div.review h4 {
+    font-size: 1rem;
+}
+
+div.main div.review.favorited {
+    background-color: lightyellow;
+}
+
+div.form-element {
+  margin-top: 10px;
+}
+div.form-element > label {
+  display: block;
+}
+div.form-element > input, div.form-element > select {
+  height: 30px;
+  width: 300px;
+}
+div.form-element > textarea {
+  height: 60px;
+  width: 300px;
+}
+form > input[type=button] {
+  width: 100px;
+}
+form > input[type=submit] {
+  width: 100px;
+  margin-right: 10px;
+}
+div.main div.well-display div.well span.amount:hover {
+    color: blue;
+    cursor: pointer;
+}
+
 </style>
--- module-4/32_Vue_Component_Communication/lecture-final/vue-product-reviews/src/components/ProductReview.vue
+++ module-4/32_Vue_Component_Communication/lecture-final/vue-product-reviews/src/components/ProductReview.vue
@@ -1,84 +1,14 @@
 <template>
     <div class="main">
-        <h2>Product Reviews for {{ name }}</h2>
-        <p v-show="showDescription" class="description">{{ description }}</p>
+        
+  
 
-        <div class="well-display">
-            <div class="well">
-                <span class="amount" v-on:click="filter = 0">{{ averageRating }}</span>
-                Average Rating
-            </div>
-            <div class="well">
-                <span class="amount" v-on:click="filter = 1">{{ numberOfOneStarReviews }}</span>
-                1 Star Review{{ numberOfOneStarReviews === 1 ? '' : 's' }}
-            </div>
-            <div class="well">
-                <span class="amount" v-on:click="filter = 2">{{ numberOfTwoStarReviews }}</span>
-                2 Star Review{{ numberOfTwoStarReviews === 1 ? '' : 's' }}
-            </div>
-            <div class="well">
-                <span class="amount" v-on:click="filter = 3">{{ numberOfThreeStarReviews }}</span>
-                3 Star Review{{ numberOfThreeStarReviews === 1 ? '' : 's' }}
-            </div>
-            <div class="well">
-                <span class="amount" v-on:click="filter = 4">{{ numberOfFourStarReviews }}</span>
-                4 Star Review{{ numberOfFourStarReviews === 1 ? '' : 's' }}
-            </div>
-            <div class="well">
-                <span class="amount" v-on:click="filter = 5">{{ numberOfFiveStarReviews }}</span>
-                5 Star Review{{ numberOfFiveStarReviews === 1 ? '' : 's' }}
-            </div>
-        </div>
-
-        <a href="#" v-on:click.prevent="showForm = true">Add New Review</a>
-
-        <form v-on:submit.prevent="addNewReview" v-if="showForm" >
-            <div class="form-element">
-                <label for="reviewer">Name:</label>
-                <input id="reviewer" type="text" v-model="newReview.reviewer" />
-            </div>
-            <div class="form-element">
-                <label for="title">Title:</label>
-                <input id="title" type="text" v-model="newReview.title" />
-            </div>
-            <div class="form-element">
-                <label for="rating">Rating:</label>
-                <select id="rating" v-model.number="newReview.rating">
-                    <option value="1">1 Star</option>
-                    <option value="2">2 Star</option>
-                    <option value="3">3 Star</option>
-                    <option value="4">4 Star</option>
-                    <option value="5">5 Star</option>
-                </select>
-            </div>
-            <div class="form-element">
-                <label for="review">Review:</label>
-                <textarea id="review" v-model="newReview.review"></textarea>
-            </div>
-            <input type="submit" value="Save" v-bind:disabled="!isFormValid" />
-            <input type="button" value="Cancel" v-on:click.prevent="resetForm()" />
-        </form>
-
-        <div class="no-reviews" v-show="noReviewsFound">
-            <p>No {{ filter }} Star Reviews Found</p>
-        </div>
+        
 
 
-        <div  class="review" 
-            v-bind:class="{ favorited: review.favorited }" 
-            v-for="review in filteredReviews" 
-            v-bind:key="review.id">
-            <h4>{{ review.reviewer }}</h4>
-            <div class="rating">
-                <img src="../assets/star.png" class="ratingStar"
-                    v-for="n in review.rating" v-bind:key="n"
-                    v-bind:title="review.rating + ' Star Review'"
-                 />
-            </div>
-            <h3>{{ review.title }}</h3>
-            <p>{{ review.review }}</p>
-            <p>Favorite? <input type="checkbox" v-model="review.favorited" /></p>
-        </div>
+
+
+
 
     </div>
 </template>
@@ -88,97 +18,13 @@ export default {
     name: 'product-review',
     data() {
         return {
-            name: "Cigar Parties for Dummies",
-            description: 'Host and plan the perfect cigar party for squirrels',
-            showDescription: true,
-            showForm: false,
-            filter: 0,
-            newReview: {},
-            reviews: [
-                {
-                    reviewer: 'Malcolm Gladwell',
-                    title: 'What a book!',
-                    review:
-                    "It certainly is a book. I mean, I can see that. Pages kept together with glue (I hope that's glue) and there's writing on it, in some language.",
-                    rating: 3
-                },
-                {
-                    reviewer: 'Tim Ferriss',
-                    title: 'Had a cigar party started in less than 4 hours.',
-                    review:
-                    "It should have been called the four hour cigar party. That's amazing. I have a new idea for muse because of this.",
-                    rating: 4
-                },
-                {
-                    reviewer: 'Ramit Sethi',
-                    title: 'What every new entrepreneurs needs. A door stop.',
-                    review:
-                    "When I sell my courses, I'm always telling people that if a book costs less than $20, they should just buy it. If they only learn one thing from it, it was worth it. Wish I learned something from this book.",
-                    rating: 1
-                },
-                {
-                    reviewer: 'Gary Vaynerchuk',
-                    title: 'And I thought I could write',
-                    review:
-                    "There are a lot of good, solid tips in this book. I don't want to ruin it, but prelighting all the cigars is worth the price of admission alone.",
-                    rating: 3
-                }
-            ]
-        }
-    },
-    computed: {
-        noReviewsFound() {
-            return this.filteredReviews.length === 0;
-        },
-        filteredReviews() {
-            // Filter the reviews based on a rating
-            // return all reviews if that rating is 0
-            return this.reviews.filter( review => {
-                return this.filter === 0 ? true : this.filter === review.rating;
-            });
-        },
-        averageRating() {
-            let sum = this.reviews.reduce( (currentSum, review) => {
-                return currentSum + review.rating;
-            }, 0);
+           
 
-            return (sum / this.reviews.length).toFixed(2);
-        },
-        numberOfOneStarReviews() {
-            return this.numberOfReviewsForRating(1);
-        },
-        numberOfTwoStarReviews() {
-            return this.numberOfReviewsForRating(2);
-        },
-        numberOfThreeStarReviews() {
-            return this.numberOfReviewsForRating(3);
-        },
-        numberOfFourStarReviews() {
-            return this.numberOfReviewsForRating(4);
-        },
-        numberOfFiveStarReviews() {
-            return this.numberOfReviewsForRating(5);
-        },
-        isFormValid() {
-            return this.newReview.reviewer && this.newReview.title &&
-                this.newReview.rating && this.newReview.review;
+    
+            
         }
     },
-    methods: {
-        numberOfReviewsForRating(starRating) {
-            return this.reviews.reduce( (count, review) =>{
-                return count + ( review.rating === starRating );
-            }, 0);
-        },
-        addNewReview() {
-            this.reviews.unshift(this.newReview);
-            this.resetForm();
-        },
-        resetForm() {
-            this.newReview = {};
-            this.showForm = false;
-        }
-    }
+    
 }
 </script>
 
