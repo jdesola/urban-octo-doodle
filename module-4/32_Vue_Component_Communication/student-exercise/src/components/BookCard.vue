043b34530cf6212daf6b58c47351a1fddf299ff5
Joshua DeSola <joshdesola@gmail.com>
2020-11-24 23:36:54 -0500

    Submitting Week 11 Day 3 Exercises

--- module-4/32_Vue_Component_Communication/student-exercise/src/components/BookCard.vue
+++ module-4/32_Vue_Component_Communication/student-exercise/src/components/BookCard.vue
@@ -1,16 +1,24 @@
 <template>
-  <div class="card">
-
-    <!-- Please leave <img> commented out until directed to remove open and close comment tags in the README.
+  <div class="card" v-bind:class="{'read' : book.read}">
+      <h2 class='book-title'>{{ book.title }} </h2>
+    <!-- Please leave <img> commented out until directed to remove open and close comment tags in the README.-->
     <img v-if="book.isbn" v-bind:src="'http://covers.openlibrary.org/b/isbn/' + book.isbn + '-M.jpg'" />
-    -->
+    <h3 class='book-author'>{{ book.author }} </h3>
     
+    <button v-on:click='markRead(book)' v-text="(book.read === false) ? 'Mark Read' : 'Mark Unread'" ></button>
+
   </div>
 </template>
 
 <script>
 export default {
-    name: 'book-card'
+    name: 'book-card',
+    props: ['book'],
+    methods: {
+        markRead(book) {
+            this.$store.commit('FLIP_READ', book);
+        }
+    }
 }
 </script>
 
--- module-4/32_Vue_Component_Communication/student-exercise/src/components/ReadingList.vue
+++ module-4/32_Vue_Component_Communication/student-exercise/src/components/ReadingList.vue
@@ -1,13 +1,21 @@
 <template>
   <div class="book-container">
-
+      <book-card v-for="book in allBooks" v-bind:key="book.isbn" v-bind:book="book"/>
   </div>
 </template>
 
 <script>
+import BookCard from './BookCard.vue'
 
 export default {
-    name: 'reading-list'
+  components: { BookCard },
+    name: 'reading-list',
+    computed: {
+      allBooks() { 
+        const books = this.$store.state.books;
+        return books;
+      }
+    }
 }
 </script>
 
