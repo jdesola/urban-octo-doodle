0a9275855e1d50374838b6813216592f0e7b6681
Joshua DeSola <joshdesola@gmail.com>
2020-11-30 10:07:54 -0500

    fix

--- module-4/40_Vue_Routing/student-tutorial/src/views/Portfolio.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div>
-      <h1>My Portfolio</h1>
-      <ul>
-          <li><a href="https://www.linkedin.com/in/joshua-desola">My LinkedIn</a></li>
-          <li><a href="https://github.com/jdesola">My GitHub</a></li>
-      </ul>
-  </div>
-</template>
